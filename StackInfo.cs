f25be9a461756d84508fa0d4f8683506d6b88e07
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-04-28 16:02:58 +0200

    Add regions to StackTrace class

--- StackInfo.cs
+++ StackInfo.cs
@@ -4,68 +4,73 @@ using System.Diagnostics;
 
 namespace ATP.LoggingTools {
 
-	public class StackInfo {
+    public class StackInfo {
+        #region FIELDS
+        private StackTrace stackTrace = new StackTrace();
+        private StackFrame frame;
+        private MethodBase method;
+        private Type classType;
+        #endregion
 
-		private StackTrace stackTrace = new StackTrace();
-		private StackFrame frame;
-		private MethodBase method;
-		private Type classType;
+        #region PROPERTIES
+        public string MethodName {
+            get {
+                if (frame != null) {
+                    return method.Name;
+                }
+                return "[Method info is not available]";
+            }
+        }
 
-		public string MethodName {
-			get {
-				if (frame != null) {
-					return method.Name;
-				}
-				return "[Method info is not available]";
-			}
-		}
+        public string MethodSignature {
+            get {
+                if (frame != null) {
+                    return method.ToString();
+                }
+                return "[Method info is not available]";
+            }
+        }
 
-		public string MethodSignature {
-			get {
-				if (frame != null) {
-					return method.ToString();
-				}
-				return "[Method info is not available]";
-			}
-		}
+        public string ClassName {
+            get {
+                // Make sure that there's a frame to get the info from.
+                if (frame != null) {
+                    return classType.Name;
+                }
+                return "[Class info is not available]";
+            }
+        }
 
-		public string ClassName {
-			get {
-				// Make sure that there's a frame to get the info from.
-				if (frame != null) {
-					return classType.Name;
-				}
-				return "[Class info is not available]";
-			}
-		}
+        public string QualifiedClassName {
+            get {
+                // Make sure that there's a frame to get the info from.
+                if (frame != null) {
+                    return classType.ToString();
+                }
+                return "[Class info is not available]";
+            }
+        }
 
-		public string QualifiedClassName {
-			get {
-				// Make sure that there's a frame to get the info from.
-				if (frame != null) {
-					return classType.ToString();
-				}
-				return "[Class info is not available]";
-			}
-		}
+        public int FrameCount {
+            get {
+                return stackTrace.FrameCount;
+            }
+        }
+        #endregion
 
-		public int FrameCount {
-			get {
-				return stackTrace.FrameCount;
-			}
-		}
+        #region METHODS
+        public StackInfo(int frameIndex) {
+            frame = stackTrace.GetFrame(frameIndex);
 
-		public StackInfo (int frameIndex) {
-			frame = stackTrace.GetFrame(frameIndex);
-
-			// Frame can be null.
-			try {
-				method = frame.GetMethod();
-				classType = method.DeclaringType;
-			}
-			catch (NullReferenceException e) {
-				UnityEngine.Debug.LogWarning("Frame not found: " + e);
-			}
-		}
-	}
+            // Frame can be null.
+            try {
+                method = frame.GetMethod();
+                classType = method.DeclaringType;
+            }
+            catch (NullReferenceException e) {
+                UnityEngine.Debug.LogWarning("Frame not found: " + e);
+            }
+        }
+        #endregion
+    }
 }

7996567ae6295e99b84a8708dcd70cef0b8f3cb2
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-04-28 15:02:46 +0200

    Implement "Append" dropdown

--- Editor/LoggerEditor.cs
+++ Editor/LoggerEditor.cs
@@ -123,21 +123,7 @@ namespace ATP.LoggingTools {
                     new GUIContent(
                         "Indent On",
                         ""));
-            EditorGUILayout.PropertyField(
-                    showTimestamp,
-                    new GUIContent(
-                        "Add Timestamp",
-                        ""));
-            EditorGUILayout.PropertyField(
-                    appendClassName,
-                    new GUIContent(
-                        "Append Class Name",
-                        "Append class name to every log message."));
-            EditorGUILayout.PropertyField(
-                    appendCallerClassName,
-                    new GUIContent(
-                        "Append Caller Name",
-                        "Append caller class name to every log message."));
+            
             EditorGUILayout.PropertyField(
                     fullyQualifiedClassName,
                     new GUIContent(

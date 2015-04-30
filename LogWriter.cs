5c93b96d360d8555d83d98486172dbdf88449b4c
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-04-30 16:25:27 +0200

    Cleanup LogWriter class

--- LogWriter.cs
+++ LogWriter.cs
@@ -6,38 +6,47 @@ namespace FileLogger {
 
     /// Holds and writes to file logged messages.
     public class LogWriter {
+        #region Delegates
 
         /// Event called on file write.
         public delegate void WriteEventHandler(object sender, EventArgs e);
-        public static event WriteEventHandler WriteEvent;
 
-        ///private bool prevLoggingEnabled = false;
-        private StreamWriter writer;
-        /// Log cache as a list. Each element is one log msg.
-        private string[] logCache;
+        #endregion
+
         /// Initial size of the cache array.
-        ///
+        /// 
         /// This value is also used to resize the array when it's filled up.
         private int initArraySize = 1000000;
-        /// 'logCacheArray' index for next log message.
-        private int logIdx;
+
+        /// Log cache as a list. Each element is one log msg.
+        private string[] logCache;
+
         /// Amount of messages logged during lifetime of the object.
         private int loggedMessages;
+
+        /// 'logCacheArray' index for next log message.
+        private int logIdx;
+
+        /// private bool prevLoggingEnabled = false;
+        private StreamWriter writer;
 
         public int InitArraySize {
             set { initArraySize = value; }
         }
+
         public int LoggedMessages {
             get { return loggedMessages; }
             set { loggedMessages = value; }
         }
 
+        public static event WriteEventHandler WriteEvent;
+
         /// Save log message to cache.
         public void Add(string message, bool echoToConsole) {
             // Initialize array if not initialized already.
             if (
-                    logCache == null ||
-                    logCache.Length == 0) {
+                logCache == null ||
+                logCache.Length == 0) {
                 logCache = new string[initArraySize];
             }
             // Cache message.
@@ -51,8 +60,8 @@ namespace FileLogger {
             // Resize array when needed.
             if (logIdx == logCache.Length) {
                 Array.Resize(
-                        ref logCache,
-                        logCache.Length + initArraySize);
+                    ref logCache,
+                    logCache.Length + initArraySize);
                 Debug.Log("Array resized to: " + logCache.Length);
             }
         }
@@ -60,15 +69,20 @@ namespace FileLogger {
         public void WriteAll(string filePath, bool append) {
             // Create stream writer used to write log cache to file.
             using (writer = new StreamWriter(filePath, append)) {
-                for (int i = 0; i < logIdx; i++) {
+                for (var i = 0; i < logIdx; i++) {
                     // write log to the file.
                     writer.WriteLine(logCache[i]);
                 }
             }
             // Inform that file has been saved.
-            DateTime now = DateTime.Now;
-            Debug.Log(string.Format("[{0:H:mm:ss}] Logs written: {1}.", now, loggedMessages));
-            // Next logs save again at the beginning of the 'logCacheArray' field.
+            var now = DateTime.Now;
+            Debug.Log(
+                string.Format(
+                    "[{0:H:mm:ss}] Logs written: {1}.",
+                    now,
+                    loggedMessages));
+            // Next logs save again at the beginning of the 'logCacheArray'
+            // field.
             logIdx = 0;
             loggedMessages = 0;
         }
@@ -76,12 +90,12 @@ namespace FileLogger {
         public void WriteLast(string filePath) {
             // Create stream writer used to write log cache to file.
             using (writer = new StreamWriter(filePath, true)) {
-                int lastCachedMsgIdx = logIdx - 1;
+                var lastCachedMsgIdx = logIdx - 1;
                 // Write to file last cached message.
                 writer.WriteLine(logCache[lastCachedMsgIdx]);
             }
             // Fire event.
-            EventArgs e = new EventArgs();
+            var e = new EventArgs();
             OnWriteEvent(e);
         }
 
@@ -90,5 +104,7 @@ namespace FileLogger {
                 WriteEvent(this, e);
             }
         }
+
     }
+
 }
